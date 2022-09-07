76bbd7c14465485296dee771510d2339f19e7be5
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-09-07 20:58:29 -0300

    main

---
+++ Calculadora de media/script.js
@@ -0,0 +1,15 @@
+/* var nota1 = 6
+var nota2 = 9
+var nota3 = 9 
+var nota4 = 10
+
+var media = (nota1+nota2+nota3+nota4) /4
+
+console.log(media) */
+
+function media (n1,n2,n3,n4){
+    var me =(n1+n2+n3+n4)/4;
+    console.log(me);
+}
+media(10,5,10,5)
+
