1ce7782d05ee0b9c459fe8ccbdd69f30f5461541
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-09-09 14:26:44 -0300

    main

--- Calculadora de media/script.js
+++ Calculadora de media/script.js
@@ -15,11 +15,32 @@ media(10,5,10,5)
 
  */
 
-var nota1 = 6.5233
+function notaFinal(){
+    const notas = document.querySelectorAll("#nota");
+    var valores = [].map.call(notas, function (input) {
+        return input.value;
+      });
+
+      var valoresInt = [];
+      var soma = 0;
+
+      for (var i = 0; i < valores.length; i++){
+         valoresInt.push(parseFloat(valores[i]));
+         soma += valoresInt[i]/4;
+      }
+     
+    document.getElementById("media").innerHTML = "Sua nota final  = "+ soma.toFixed(2);
+}
+   /*  console.log(soma); */
+   
+    
+  
+
+/* var nota1 = 6.5233
 var nota2 = 9.5633
 var nota3 = 9.855
 var nota4 = 10.202
 
 var notaFinal =(nota1 + nota2 + nota3 + nota4) /4
 
-console.log(notaFinal.toFixed(0))
+console.log(notaFinal.toFixed(0)) */
