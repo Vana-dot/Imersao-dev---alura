b6c41061e604367aff629cc5c34dd722fa365a4b
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-09-10 15:43:12 -0300

    main

--- Conversor de temperaturas/script.js
+++ Conversor de temperaturas/script.js
@@ -1,15 +1,31 @@
+// Pegando os valores selecionados no select
+var select = document.getElementById("temperaturas");
+var optionValor = select.options[select.selectedIndex].value;
+
+
+// Função para converter Celsius para fahrenheit, fahrenheit para kelvius e kelvius para celsius
 function ConverterTemperaturas(){
+    
     const temperaturaInput = document.querySelector("#temperatura").value;
     var temperaturaConvertido = ((temperaturaInput * 9)/5) + 32;
+    var temperaturaConvertidoKel =((temperaturaInput-32)*5)/9 + 273.15;
+    var temperaturaConvertidoCelsius = temperaturaInput - 273.15;
 
-    if (document.getElementById("temperaturaConvertida").style.display === "none"){
+    if(document.getElementById("temperaturaConvertida").style.display === "none" && optionValor === "celsius"){
         document.getElementById("temperaturaConvertida").style.display = "inline";
          document.getElementById("temperaturaConvertida").innerHTML = temperaturaConvertido.toFixed(2)+ " °F";
     }
-    else{
-        document.getElementById("temperaturaConvertida").style.display = "none";
+    else if(document.getElementById("temperaturaConvertida").style.display === "none" && optionValor === "fahrenheit"){
+        document.getElementById("temperaturaConvertida").style.display = "inline";
+        document.getElementById("temperaturaConvertida").innerHTML = temperaturaConvertidoKel.toFixed(2)+ " °K";
     }
-         
 
+    else if(document.getElementById("temperaturaConvertida").style.display === "none" && optionValor === "kelvius"){
+        document.getElementById("temperaturaConvertida").style.display = "inline";
+        document.getElementById("temperaturaConvertida").innerHTML = temperaturaConvertidoCelsius.toFixed(2)+ " °C";
+    }
+    
 }
+
+
     
