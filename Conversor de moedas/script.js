be776c4049fc00938a756ff036d1050beab26c77
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-09-09 21:02:40 -0300

    main

--- Conversor de moedas/script.js
+++ Conversor de moedas/script.js
@@ -1,5 +1,19 @@
 function Converter(){
-    const valorInput = document.querySelector("#valor").value;
+    const valorInput = document.querySelector(".valor").value;
     var moedaConvertida = valorInput * 5.21480;
-    document.getElementById("valorConvertido").innerHTML = "R$ "+ moedaConvertida.toFixed(2);
+    var moedaEmBitcon = valorInput * 0.000047;
+    document.getElementById("valorConvertido").innerHTML = "R$ "+ moedaConvertida.toFixed(2) +"<br>"+ "Em Bitcon " + moedaEmBitcon;
+
 }
+    
+
+function ConverterReal(){
+    const valorInputReal = document.querySelector(".valorReal").value;
+    var moedaConvertidaReal = valorInputReal / 5.21480;
+    var moedaEmBitconReal = valorInputReal * 0.0000091;
+    document.getElementById("valorConvertidoReal").innerHTML = "R$ "+ moedaConvertidaReal.toFixed(2) +"<br>"+ "Em Bitcon " + moedaEmBitconReal;
+    
+
+
+}
+
