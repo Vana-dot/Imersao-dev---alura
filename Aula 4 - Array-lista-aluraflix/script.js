e5cb468fbc266bade050aae36e88e1854c5db8c9
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-09-11 21:26:13 -0300

    main

--- Aula 4 - Array-lista-aluraflix/script.js
+++ Aula 4 - Array-lista-aluraflix/script.js
@@ -1,6 +1,18 @@
-var filmes = ["Yesteday", "365 DNI", "Homem aranha"];
-var filmes1 = {
-    nome: "Insta"
+//Uma lista com todos os filmes preferidos
+/* var filmes = ["Yesteday ","365 DNI","Homem-Aranha: Sem Volta para Casa"];
+
+// Criando um for que vai percorrer todos elementos dessa lista e colocar em uma lista
+for (var i = 0; i < filmes.length; i++) {
+    var element = filmes[i];
+    document.write("<ul> <li>"+i+" - "+element+"</li> </ul>");
 }
-document.write(filmes1.nome)
-console.log(filmes[0])
+
+ */
+
+//Imagens dos filmes listados na lista anteriormente
+var imagensFilmes = ["https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg", "https://br.web.img2.acsta.net/pictures/22/08/16/23/17/2934353.jpg","https://midias.correiobraziliense.com.br/_midias/jpg/2022/08/22/homem_aranha_the_more_fun_stuff-26280289.jpg"]
+
+for (let i = 0; i < imagensFilmes.length; i++) {
+    document.write("<img src= "+imagensFilmes[i]+">");
+    
+}
---
+++ Aula 5 - Funçao do aluraflix/script.js
@@ -0,0 +1,7 @@
+/* var filmes = ["https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg", "https://br.web.img2.acsta.net/pictures/22/08/16/23/17/2934353.jpg","https://midias.correiobraziliense.com.br/_midias/jpg/2022/08/22/homem_aranha_the_more_fun_stuff-26280289.jpg"]
+
+for (let i = 0; i < filmes.length; i++) {
+    document.write("<img src= "+filmes[i]+">");
+    
+}
+ */
