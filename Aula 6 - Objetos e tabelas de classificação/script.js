b3a43bdebde117a34bab38d8d581483a8e5ed8ae
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-10-09 20:33:42 -0300

    main

--- Aula 6 - Objetos e tabelas de classificação/script.js
+++ Aula 6 - Objetos e tabelas de classificação/script.js
@@ -1,29 +1,58 @@
 var vana = {
     nome:"Vana",
-    vitorias:0,
+    vitorias:2,
+    empates:1,
     derrotas:1,
-    empates:0,
-    pontos:0
+    pontos: 0
 
 }
 var jose = {
     nome:"José",
     vitorias:1,
-    derrotas:0,
-    empates:0,
+    empates:1,
+    derrotas:2,
     pontos:0
 
 
 }
 
-function adicionarVitoria(){
-
+function calcularPontos(jogador){
+    var pontos = (jogador.vitorias*3)+(jogador.empates)
+    return pontos
 }
 
-function adicionarDerrota(){
+vana.pontos = calcularPontos(vana);
+jose.pontos = calcularPontos(jose);
 
+
+var jogadores = [vana,jose];
+
+function exibeJogadoresNaTela(jogadores){
+  var elemento = ""
+
+  for (var i = 0; i < jogadores.length; i++){
+    elemento += "<tr><td>"+jogadores[i].nome+"</td>"
+    elemento += "<td>"+jogadores[i].vitorias+"</td>"
+    elemento += "<td>"+jogadores[i].empates+"</td>"
+    elemento += "<td>"+jogadores[i].derrotas+"</td>"
+    elemento += "<td>"+jogadores[i].pontos+"</td>"
+    elemento += '<td><button onClick="adicionarVitoria()">Vitória</button></td>'
+    elemento += '<td><button onClick="adicionarEmpate()">Empate</button></td>'
+    elemento +=  '<td><button onClick="adicionarDerrota()">Derrota</button></td>'
+    elemento += "</tr>"
+  }
+  document.getElementById("tabelaJogadores").innerHTML = elemento;
 }
 
-function adicionarEmpate(){
+exibeJogadoresNaTela(jogadores);
+// function adicionarVitoria(){
 
-}
+// }
+
+// function adicionarDerrota(){
+
+// }
+
+// function adicionarEmpate(){
+
+// }
