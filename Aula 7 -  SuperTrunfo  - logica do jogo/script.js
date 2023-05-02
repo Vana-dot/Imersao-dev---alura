b45661f2d3dbad4b1b8c09d6c3690278617bd855
Geovana venancio <geovanavenancio5556@gmail.com>
2023-05-02 20:18:09 -0300

    main

--- Aula 7 -  SuperTrunfo  - logica do jogo/script.js
+++ Aula 7 -  SuperTrunfo  - logica do jogo/script.js
@@ -1,10 +1,51 @@
-var cartas = {
+var carta1 = {
+   nome: 'Bulbasauro',
+   atributos: {
+    ataque: 7,
+    defesa: 8,
+    magia: 6,
+   }
+};
+
+var carta2 = {
+    nome: 'Darth Vader',
+    atributos: {
+        ataque: 9,
+        defesa: 10,
+        magia: 2,
+    },
+};
+
+var carta3 = {
+    nome: 'shiryu de dragão',
+    atributos: {
+        ataque: 5,
+        defesa: 10,
+        magia: 10,
+    },
+};
+
+var cartas = [carta1, carta2, carta3];
+
+var cartaMaquina;
+var cartaJogador;
+
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length);
+    cartaMaquina = cartas[numeroCartaMaquina];
+    console.log(cartaMaquina);
+   
+    
+   var numeroCartaJogador = parseInt(Math.random() * cartas.length);
+   while (numeroCartaMaquina == numeroCartaJogador){
+     numeroCartaJogador = parseInt(Math.random() * cartas.length);
+   }
+    cartaJogador = cartas[numeroCartaJogador];
+    console.log(cartaJogador);
 
 }
 
-let carta = "vana";
-carta = "igor";
 
-const car = "Mano";
-car = "minha";
-console.log(car)
+
+sortearCarta();    
